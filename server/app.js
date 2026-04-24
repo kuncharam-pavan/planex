9888baaa2b8a3fa6e0ddb8f5f83aaff833122729
kuncharam-pavan <kuncharampavan2580@gmail.com>
2026-04-24 09:23:11 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -8,9 +8,14 @@ const registerroute = require("./routes/registrationroute.js")
 const uploadfiles = require("./routes/uploadfileroutes.js")
 const {apinotfound} = require("./middleware/apinotfound.js")
 const {globalerror}=require("./middleware/globalerrmiddleware.js")
+// cors is a middle ware for update the new events in the frontend
+const cors= require("cors")
 const {connectdb} = require("./configs/mongodb.js")
 connectdb()
 
+
+
+app.use(cors({origin:["http://localhost:5173"]}))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use("/api/auth",userrouter)
